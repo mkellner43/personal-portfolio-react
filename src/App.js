67a230d7d5c9c96e2d55c537bcb9e8f315071595
mkellner43 <matthew.kellner@outlook.com>
2022-12-28 10:13:01 -0600

    created home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <>
+    <Nav />
+    <Routes>
+      <Route default path='/' element={<Home />} />
+    </Routes>
+    </>
+    
   );
 }
 
