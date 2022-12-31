e876d1c70fdab099cc40b418e6a9baa3e768a2da
mkellner43 <matthew.kellner@outlook.com>
2022-12-31 13:46:51 -0600

    moved SVG draw animation to home

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -93,7 +93,7 @@ const Nav = ({isOpen, setIsOpen}) => {
       <motion.div animate={{x: -5, zIndex: 1, color: ['#000','#fff'], position:'absolute', transition: {duration: 1,delay: 0.2, repeat: Infinity, repeatType: 'reverse'}}}>
         <FontAwesomeIcon icon={faChevronLeft} style={{color: 'inherit', zIndex: 1, position:'absolute', left: 130, top: 20}}/>
       </motion.div>
-      <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" height={20} width={20} style={{zIndex: 1, position: 'absolute', top: 50, left: 36}}>
+      {/* <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" height={20} width={20} style={{zIndex: 1, position: 'absolute', top: 50, left: 36}}>
         <motion.path 
           strokeLinecap="round"
           strokeWidth={20}
@@ -103,7 +103,7 @@ const Nav = ({isOpen, setIsOpen}) => {
           style={{rotate: 90}}
           transition={transition} stroke='#fff' d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
         />
-      </motion.svg>
+      </motion.svg> */}
     </>
   )
 }
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,13 +1,41 @@
 import './style.scss';
-import MemoryGame from '../../Images/memoryGame.png'
+import memoryGame from '../../Images/memoryGame.png';
+import myShop from '../../Images/myShop.png';
+import cvBuilder from '../../Images/cvBuilder.png';
+import weatherChecker from '../../Images/weatherChecker.png';
+import ticTacToe from '../../Images/ticTacToe.png';
+
+import { motion } from 'framer-motion';
 
 const Projects = () => {
   return (
-    <section className="projects-container">
-      <div>
-        <a href="https://mkellner43.github.io/memory-game/" target="_blank" rel="noopener noreferrer">
-          <img src={MemoryGame} alt="memory game" />
-        </a>
+    <section className="projects-wrapper">
+      <div className='projects-container'>
+        <div>
+          <a href="https://mkellner43.github.io/memory-game/" target="_blank" rel="noopener noreferrer">
+            <motion.img whileInView={{opacity: 1, transition: {duration: 2}}} viewport={{ once: true, amount: 0.8 }} src={memoryGame} alt="memory game" />
+          </a>
+        </div>
+        <div>
+          <a href="https://mkellner43.github.io/shop_react/" target="_blank" rel="noopener noreferrer">
+            <motion.img whileInView={{opacity: 1, transition: { duration: 1}}} viewport={{ once: true, amount: 0.8 }} src={myShop} alt="shop" />
+          </a>
+        </div>
+        <div>
+          <a href="https://mkellner43.github.io/cv-project/" target="_blank" rel="noopener noreferrer">
+            <motion.img whileInView={{opacity: 1, transition: {duration: 2}}} viewport={{ once: true, amount: 0.8 }} src={cvBuilder} alt="cv builder" />
+          </a>
+        </div>
+        <div>
+          <a href="https://mkellner43.github.io/weather_api/" target="_blank" rel="noopener noreferrer">
+            <motion.img whileInView={{opacity: 1, transition: {duration: 1}}} viewport={{ once: true, amount: 0.8 }} src={weatherChecker} alt="weather checker" />
+          </a>
+        </div>
+        <div>
+          <a href="https://mkellner43.github.io/ticTacToeJs/" target="_blank" rel="noopener noreferrer">
+            <motion.img whileInView={{opacity: 1, transition: {duration: 1}}} viewport={{ once: true, amount: 0.8 }} src={ticTacToe} alt="weather checker" />
+          </a>
+        </div>
       </div>
     </section>
   )
