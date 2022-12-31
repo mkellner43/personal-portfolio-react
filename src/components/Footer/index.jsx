bf4b6328450f45bbdd9a38646e24bf4fb7f57be8
mkellner43 <matthew.kellner@outlook.com>
2022-12-30 18:03:40 -0600

    added overflow scroll to root

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,28 @@
 import './style.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
     <footer>
-      <FontAwesomeIcon icon={faLinkedin} />
-      <FontAwesomeIcon icon={faGithub} />
-      <FontAwesomeIcon icon={faFacebook} />
+      <motion.div className='links'>
+        <motion.div whileHover={{scale: 1.1}}>
+          <motion.a href='https://linkedin.com/in/mkellner43/' target={'_blank'} rel='noreferrer'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </motion.a>
+        </motion.div>
+        <motion.div whileHover={{scale: 1.1}}>
+        <motion.a href='https://github.com/mkellner43/' target={'_blank'} rel='noreferrer'>
+          <FontAwesomeIcon icon={faGithub} />
+        </motion.a>
+        </motion.div>
+        <motion.div whileHover={{scale: 1.1}}>
+        <motion.a href='https://facebook.com/matt.kellner1/' target={'_blank'} rel='noreferrer'>
+          <FontAwesomeIcon icon={faFacebook}/>
+        </motion.a>
+        </motion.div>
+      </motion.div>
     </footer>
   )
 };
