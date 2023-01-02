56db8d078e1ef92d15aa9601e8b1fd826d3b1ff1
mkellner43 <matthew.kellner@outlook.com>
2023-01-02 13:42:30 -0600

    updated arrow animtion

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -21,7 +21,7 @@ const Home = ({navIsOpen}) => {
       delay: 0.4,
     }
   }
-  const transition = { duration: 2, yoyo: Infinity, ease: "easeInOut" }
+  const transition = { duration: 2, repeat: Infinity, repeatType: 'reverse', ease: "easeInOut" }
 
  return (
   <motion.section className='home-container'  animate={navIsOpen ? open : closed} initial={false}>
@@ -40,12 +40,12 @@ const Home = ({navIsOpen}) => {
         animate={{opacity: 1, boxShadow: '0 0 20px', transition: {duration: 2}}} 
         whileHover={{rotate: 10, transition: {duration: 2}}} />
     </div>
-    <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" height={40} width={40} style={{zIndex: 1, position: 'relative', top: '20vh', left: '48.5%'}}>
+    <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500" height={40} width={40} style={{zIndex: 1, position: 'relative', top: '10vh', left: '48.5%'}} initial={{y: 0}} animate={{y: 100, transition: {duration: 2, repeat: Infinity, repeatType: 'reverse'}}}>
       <motion.path 
         strokeLinecap="round"
         strokeWidth={20}
         initial={{ pathLength: 0 }}
-        animate={{ pathLength: 1 }}
+        animate={{ pathLength: 1}}
         fill="none"
         style={{rotate: -90}}
         transition={transition} stroke='#fff' d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
@@ -59,7 +59,6 @@ const Home = ({navIsOpen}) => {
 
 export default Home;
 
-// make footer appear with nav (:
 // make arrow bounce when animation is drawn
 // dynamic background to surround name and pic (:
 // in about create cool animations to display what tech you know
