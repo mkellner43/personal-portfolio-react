553f8b180b88cd3e09db857d1a4da9464d3462bc
mkellner43 <matthew.kellner@outlook.com>
2023-01-03 10:00:07 -0600

    updated UI

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,22 @@
+import './style.scss';
+
+const Background = () => {
+  return (
+    <div className='circles'>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+    </div>
+  )
+}
+
+export default Background;
