e605a8a6284a07576a4c2a9a1c4d1347c9081262
mkellner43 <matthew.kellner@outlook.com>
2023-01-05 17:59:53 -0600

    updated footer link UI

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,25 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { motion } from 'framer-motion';
 
-const Footer = ({className}) => {
+const Footer = () => {
   return (
-    <footer className={className ? className : ''}>
+    <footer>
       <motion.div className='links'>
-        <motion.div whileHover={{scale: 1.1}}>
-          <motion.a href='https://linkedin.com/in/mkellner43/' target={'_blank'} rel='noreferrer'>
+          <motion.a whileHover={{scale: 1.1}} href='https://linkedin.com/in/mkellner43/' target={'_blank'} rel='noreferrer'>
             <FontAwesomeIcon icon={faLinkedin} />
           </motion.a>
-        </motion.div>
-        <motion.div whileHover={{scale: 1.1}}>
-        <motion.a href='https://github.com/mkellner43/' target={'_blank'} rel='noreferrer'>
+        <motion.a whileHover={{scale: 1.1}} href='https://github.com/mkellner43/' target={'_blank'} rel='noreferrer'>
           <FontAwesomeIcon icon={faGithub} />
         </motion.a>
-        </motion.div>
-        <motion.div whileHover={{scale: 1.1}}>
-        <motion.a href='https://facebook.com/matt.kellner1/' target={'_blank'} rel='noreferrer'>
+        <motion.a whileHover={{scale: 1.1}} href='https://facebook.com/matt.kellner1/' target={'_blank'} rel='noreferrer'>
           <FontAwesomeIcon icon={faFacebook}/>
         </motion.a>
-        </motion.div>
       </motion.div>
     </footer>
   )
