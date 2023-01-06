04cc1aace09b0769bde9f9fc8cc313ca665cedde
mkellner43 <matthew.kellner@outlook.com>
2023-01-06 10:17:57 -0600

    updated UI

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
   return (
     <footer>
       <motion.div className='links'>
-          <motion.a whileHover={{scale: 1.1}} href='https://linkedin.com/in/mkellner43/' target={'_blank'} rel='noreferrer'>
-            <FontAwesomeIcon icon={faLinkedin} />
+          <motion.a whileHover={{scale: 1.1, color: '#0072b1'}} href='https://linkedin.com/in/mkellner43/' target={'_blank'} rel='noreferrer'>
+            <FontAwesomeIcon icon={faLinkedin}/>
           </motion.a>
-        <motion.a whileHover={{scale: 1.1}} href='https://github.com/mkellner43/' target={'_blank'} rel='noreferrer'>
+        <motion.a whileHover={{scale: 1.1, color: '#333'}} href='https://github.com/mkellner43/' target={'_blank'} rel='noreferrer'>
           <FontAwesomeIcon icon={faGithub} />
         </motion.a>
-        <motion.a whileHover={{scale: 1.1}} href='https://facebook.com/matt.kellner1/' target={'_blank'} rel='noreferrer'>
+        <motion.a whileHover={{scale: 1.1, color: '#3b5998'}} href='https://facebook.com/matt.kellner1/' target={'_blank'} rel='noreferrer'>
           <FontAwesomeIcon icon={faFacebook}/>
         </motion.a>
       </motion.div>
