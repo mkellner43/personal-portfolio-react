4da637f2a8eba75400e61a3a38c894b8f93c53fa
mkellner43 <matthew.kellner@outlook.com>
2023-01-19 11:18:14 -0600

    added localStorage theme saving

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import Contact from './components/Contact';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
-  const [darkTheme, setDarkTheme] = useState(false);
-  
+  const [darkTheme, setDarkTheme] = useState(localStorage.getItem('darkTheme') === 'true' ? true : false);
   useEffect(() => {
     const root = document.documentElement;
-    root?.style.setProperty("--primary-color",darkTheme ? "#000401" : "#B0B0B0");
+    root?.style.setProperty("--primary-color", darkTheme ? "#000401" : "#B0B0B0");
     root?.style.setProperty("--secondary-color", darkTheme ? "#E5E8E8" : "#262833");
-    root?.style.setProperty("--background-color", darkTheme ? '#4E545C' : '#F5F5F5')
-
+    root?.style.setProperty("--background-color", darkTheme ? '#4E545C' : '#F5F5F5');
+    localStorage.setItem('darkTheme', darkTheme)
   }, [darkTheme]);
 
   return (
