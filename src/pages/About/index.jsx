2a4f1db6e3d55036fcba0f4b3352d640bba72b9e
mkellner43 <matthew.kellner@outlook.com>
2023-07-25 20:02:27 -0500

    updated spacing

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,39 +6,38 @@ import resume from "../../Images/resume.pdf";
 const About = () => {
   return (
     <motion.section className="flex flex-col w-screen justify-center items-center pt-20 mt-10 gap-16 bg-slate-200 dark:bg-stone-500 dark:text-white">
-      <div className="flex flex-col justify-center items-center max-w-xl overflow-x-clip">
-
-      <motion.h1
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-        className="text-5xl text-center font-nunito"
+      <div className="flex flex-col justify-center items-center max-w-xl overflow-x-clip px-5 gap-5">
+        <motion.h1
+          initial={{ y: -100, opacity: 0 }}
+          animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          className="text-5xl text-center font-nunito"
         >
-        About me.
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, transition: { duration: 1 } }}
-        viewport={{ once: true }}
-        className="text-justify font-nunito text-xl"
+          About me.
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, transition: { duration: 1 } }}
+          viewport={{ once: true }}
+          className="text-justify font-nunito text-xl"
         >
-        Hi, I'm Matt, a full stack web developer with 2 years of experience
-        building websites. Much of my growth came through my undying curiosity
-        for how technology works. I enjoy challenging myself to learn new
-        technologies. Lifetime learning and continuous growth is my passion!
-      </motion.p>
-      <motion.div className="flex justify-center items-center">
-        <motion.a
-          href={resume}
-          download
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.9 }}
-          className="text-lg font-nunito bg-blue-500 p-2 rounded-full"
+          Hi, I'm Matt, a full stack web developer with 2 years of experience
+          building websites. Much of my growth came through my undying curiosity
+          for how technology works. I enjoy challenging myself to learn new
+          technologies. Lifetime learning and continuous growth is my passion!
+        </motion.p>
+        <motion.div className="flex justify-center items-center">
+          <motion.a
+            href={resume}
+            download
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            className="text-lg font-nunito bg-blue-500 p-2 rounded-full"
           >
-          Download Resume
-        </motion.a>
-      </motion.div>
-      <Skills />
-          </div>
+            Download Resume
+          </motion.a>
+        </motion.div>
+        <Skills />
+      </div>
       <Footer />
     </motion.section>
   );
