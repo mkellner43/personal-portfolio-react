9eb7013ecdd7c2e84a3658b2f7fcdd9877272f66
mkellner43 <matthew.kellner@outlook.com>
2023-11-02 17:57:02 -0500

    updated animation speed

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,34 +1,47 @@
 import Scroll from "./Scroll";
 import { frontend, backend, other } from "./data";
 import { Typewriter } from "../Typewriter";
+import { Typography } from "@mui/material";
+import { motion } from "framer-motion";
 
 const Skills = ({ aboutContainer }) => {
   return (
     <section className="flex flex-col gap-5 w-full relative pb-20">
       <div className="min-h-[60px]">
-        <Typewriter text="Skills." variant="h2" component="h2" delay={5000} />
+        <Typewriter text="Skills." variant="h2" component="h2" delay={1000} />
       </div>
-      <div className="min-h-[35px]">
-        <Typewriter text="Frontend." variant="h6" component="h3" delay={6000} />
-      </div>
-      <Scroll
-        data={frontend}
-        baseVelocity={-2}
-        aboutContainer={aboutContainer}
-      />
-      <div className="min-h-[35px]">
-        <Typewriter text="Backend." variant="h6" component="h3" delay={7000} />
-      </div>
-      <Scroll data={backend} baseVelocity={2} aboutContainer={aboutContainer} />
-      <div className="min-h-[35px]">
-        <Typewriter
-          text="Miscellaneous."
-          variant="h6"
-          component="h3"
-          delay={8000}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 1.2, duration: 1.5, ease: "easeInOut" },
+        }}
+      >
+        <Typography variant="h6" component="h3">
+          Frontend
+        </Typography>
+        <Scroll
+          data={frontend}
+          baseVelocity={-2}
+          aboutContainer={aboutContainer}
         />
-      </div>
-      <Scroll data={other} baseVelocity={-2} aboutContainer={aboutContainer} />
+        <Typography variant="h6" component="h3">
+          Backend
+        </Typography>
+        <Scroll
+          data={backend}
+          baseVelocity={2}
+          aboutContainer={aboutContainer}
+        />
+        <Typography variant="h6" component="h3">
+          Miscellaneous
+        </Typography>
+        <Scroll
+          data={other}
+          baseVelocity={-2}
+          aboutContainer={aboutContainer}
+        />
+      </motion.div>
     </section>
   );
 };
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,29 +2,28 @@ import { motion } from "framer-motion";
 import Skills from "../../components/Skills";
 import resume from "../../Images/resume.pdf";
 import { Typewriter } from "../../components/Typewriter";
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 const About = () => {
   return (
-      <div className="flex flex-col max-w-xl w-full mt-[100px]">
-        <div className="w-full">
-          <Typewriter text="About me." variant={"h2"} component={"h1"} />
-          <div className="min-h-[32vh] sm:min-h-[22vh] py-4">
-            <Typewriter
-              text=" Hello, I'm Matt, a seasoned full-stack web developer with a track
-            record of 3 years in crafting exceptional websites. My journey in this
-            dynamic field has been fueled by an insatiable curiosity for
+    <div className="flex flex-col max-w-xl w-full mt-[100px]">
+      <div className="w-full">
+        <Typewriter text="About me." variant={"h2"} component={"h1"} />
+        <motion.div
+          className="py-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
+        >
+          <Typography variant="p" component="p">
+            Hello, I'm Matt, a seasoned full-stack web developer with a track
+            record of 3 years in crafting exceptional websites. My journey in
+            this dynamic field has been fueled by an insatiable curiosity for
             understanding the intricacies of technology. I thrive on challenging
             myself to embrace emerging technologies and methodologies, making
             continuous learning and personal growth the driving forces behind my
-            career. Welcome to my portfolio, where you can explore my passion for
-            creating innovative digital experiences!"
-              variant="body1"
-              component="p"
-              delay={1000}
-              typingSpeed={20}
-            />
-          </div>
+            career. Welcome to my portfolio, where you can explore my passion
+            for creating innovative digital experiences!"
+          </Typography>
           <motion.div className="flex justify-center items-center mt-2">
             <Button
               href={resume}
@@ -36,11 +35,12 @@ const About = () => {
               Download Resume
             </Button>
           </motion.div>
-        </div>
-        <div className="w-full overflow-hidden mt-10">
-          <Skills />
-        </div>
+        </motion.div>
       </div>
+      <div className="w-full overflow-hidden mt-10">
+        <Skills />
+      </div>
+    </div>
   );
 };
 
